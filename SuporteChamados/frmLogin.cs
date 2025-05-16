2f747e040a08a1355baa98588e509c9e73290e40
raynaralisboa345@gmail.com <raynaralisboa345@gmail.com>
2025-05-16 20:31:06 -0300

    Funcionalidade do login

--- SuporteChamados/frmLogin.cs
+++ SuporteChamados/frmLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,10 +20,42 @@ namespace SuporteChamados
 
         private void btnAcessar_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            frmprincipal Frmprincipal = new frmprincipal();
-            Frmprincipal.ShowDialog();
-            this.Show();
+            string usuario = txtUsuario.Text.Trim();
+            string senha = txtSenha.Text.Trim();
+
+            // Aqui você abre a conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection("SUA_STRING_DE_CONEXAO"))
+            {
+                conn.Open();
+                string query = "SELECT id, nome FROM usuarios WHERE login = @usuario AND senha = @senha";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@usuario", usuario);
+                    cmd.Parameters.AddWithValue("@senha", senha); // Lembre de criptografar na versão final!
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                       
+                        IdUsuario.IDUsuario = reader.GetInt32(0);
+                        IdUsuario.NomeUsuario = reader.GetString(1);
+
+                        MessageBox.Show("Login bem-sucedido!");
+
+                        
+                        this.Hide();
+                        frmprincipal Frmprincipal = new frmprincipal();
+                        Frmprincipal.ShowDialog();
+                        this.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuário ou senha inválidos!");
+                    }
+                }
+            }
         }
     }
 }
